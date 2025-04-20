4fe5a97bd11af3248a987f62269f35b3fa7412a4
Malin Sundberg <anomalins@gmail.com>
2025-04-20 15:59:13 +0200

    more changes to the design

--- BatchJobGUI/script.js
+++ BatchJobGUI/script.js
@@ -1,12 +1,22 @@
 const execButton = document.getElementById("exec");
 const popup = document.getElementById("expop");
 const overlay = document.getElementById("overlay");
+const closeButton = document.getElementById("close");
 
 execButton.addEventListener("click", () => {
     showPopup();
 })
 
+closeButton.addEventListener("click", () => {
+    closePopup();
+});
+
 function showPopup() {
     popup.style.display = "flex";
     overlay.style.display= "block";
+}
+
+function closePopup() {
+    popup.style.display="none";
+    overlay.style.display="none";
 }
